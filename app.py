bc3f3e8129a78045158a4333ceade4246bd778d9
Mia0301 <168523364+Mia0301@users.noreply.github.com>
2024-06-04 00:39:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import streamlit.components.v1 as stc 
+import matplotlib 
 import matplotlib.pyplot as plt
 import datetime
 html_temp = """
