691f4cd60e47f52cd2b051b2bbd72b2ba14911ad
Mia0301 <168523364+Mia0301@users.noreply.github.com>
2024-06-04 11:16:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import datetime
 st.set_page_config(layout = 'wide',initial_sidebar_state='expanded')
 
 st.sidebar.header('選擇開始與結束的日期, 區間:2022-01-03 至 2024-06-03')
-start_date = st.text_input('選擇開始日期 (日期格式: 2022-01-03)', '2022-01-03')
+st.sidebar.selectbox(start_date = st.text_input('選擇開始日期 (日期格式: 2022-01-03)', '2022-01-03'))
 end_date = st.text_input('選擇結束日期 (日期格式: 2024-06-03)', '2024-06-03')
 start_date = datetime.datetime.strptime(start_date,'%Y-%m-%d')
 end_date = datetime.datetime.strptime(end_date,'%Y-%m-%d')
